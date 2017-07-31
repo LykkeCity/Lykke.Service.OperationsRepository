3e44e1ab8602c5522d9d76951f1b3f47bdaf4586
atarutin <tarutin_andrew@mail.ru>
2017-07-31 22:47:47 +0200

    added new repository clients: TradeOperationsRepositoryClient, TransferOperationsRepositoryClient

--- client/Lykke.Service.OperationsRepository.Client/Abstractions/CashOperations/ITradeOperationsRepositoryClient.cs
+++ client/Lykke.Service.OperationsRepository.Client/Abstractions/CashOperations/ITradeOperationsRepositoryClient.cs
@@ -1,7 +1,22 @@
-﻿namespace Lykke.Service.OperationsRepository.Client.Abstractions.CashOperations
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Service.OperationsRepository.AutorestClient.Models;
+using Lykke.Service.OperationsRepository.Client.Models.CashOperations;
+
+namespace Lykke.Service.OperationsRepository.Client.Abstractions.CashOperations
 {
     public interface ITradeOperationsRepositoryClient
     {
-        
+        Task<ClientTradesResponse> SaveAsync(params ClientTrade[] clientTrades);
+        Task<ClientTradesResponse> GetAsync(string clientId);
+        Task<ClientTradesResponse> GetAsync(DateTime from, DateTime to);
+        Task<ClientTradeResponse> GetAsync(string clientId, string recordId);
+        Task UpdateBlockChainHashAsync(string clientId, string recordId, string hash);
+        Task SetDetectionTimeAndConfirmations(string clientId, string recordId, DateTime detectTime, int confirmations);
+        Task SetBtcTransactionAsync(string clientId, string recordId, string btcTransactionId);
+        Task SetIsSettledAsync(string clientId, string id, bool offchain);
+        Task<ClientTradesResponse> GetByMultisigAsync(string multisig);
+        Task<ClientTradesResponse> GetByMultisigsAsync(string[] multisigs);
     }
 }
---
+++ client/Lykke.Service.OperationsRepository.Client/Models/CashOperations/ClientTradeResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lykke.Service.OperationsRepository.AutorestClient.Models;
+using Microsoft.Rest;
+
+namespace Lykke.Service.OperationsRepository.Client.Models.CashOperations
+{
+    public class ClientTradeResponse: BaseCashOperationResponse
+    {
+        public ClientTrade Operation { get; set; }
+
+        public static ClientTradeResponse Prepare(HttpOperationResponse<object> apiResponse)
+        {
+            var error = apiResponse.Body as ErrorResponse;
+            var result = apiResponse.Body as ClientTrade;
+
+            if (error != null)
+            {
+                return new ClientTradeResponse
+                {
+                    Error = new ErrorModel
+                    {
+                        Message = error.ErrorMessage
+                    }
+                };
+            }
+
+            if (result != null)
+            {
+                return new ClientTradeResponse
+                {
+                    Operation = result
+                };
+            }
+
+            throw new ArgumentException("Unknown response object");
+        }
+    }
+}
