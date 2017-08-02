f24761f13eb4d1ef6613feb16368fdb12706806e
atarutin <tarutin_andrew@mail.ru>
2017-08-02 18:02:11 +0200

    added interface for transfer repository client

--- client/Lykke.Service.OperationsRepository.Client/Abstractions/CashOperations/ITransferOperationsRepositoryClient.cs
+++ client/Lykke.Service.OperationsRepository.Client/Abstractions/CashOperations/ITransferOperationsRepositoryClient.cs
@@ -1,7 +1,21 @@
-﻿namespace Lykke.Service.OperationsRepository.Client.Abstractions.CashOperations
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Service.OperationsRepository.AutorestClient.Models;
+using Lykke.Service.OperationsRepository.Client.Models.CashOperations;
+
+namespace Lykke.Service.OperationsRepository.Client.Abstractions.CashOperations
 {
     public interface ITransferOperationsRepositoryClient
     {
-        
+        Task<TransferEventResponse> RegisterAsync(TransferEvent transferEvent);
+        Task<TransferEventsResponse> GetAsync(string clientId);
+        Task<TransferEventResponse> GetAsync(string clientId, string id);
+        Task UpdateBlockChainHashAsync(string clientId, string id, string blockChainHash);
+        Task SetBtcTransactionAsync(string clientId, string id, string btcTransaction);
+        Task SetIsSettledIfExistsAsync(string clientId, string id, bool offchain);
+        Task<TransferEventsResponse> GetByHashAsync(string blockchainHash);
+        Task<TransferEventsResponse> GetByMultisigAsync(string multisig);
+        Task<TransferEventsResponse> GetByMultisigsAsync(string[] multisigs);
     }
 }
