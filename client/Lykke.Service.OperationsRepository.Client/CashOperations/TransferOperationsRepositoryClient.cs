616aaf3126f98695ddd30bd1adbb74e51a317d69
atarutin <tarutin_andrew@mail.ru>
2017-08-03 13:54:53 +0200

    added TransferOperationsRepositoryClient.cs

---
+++ client/Lykke.Service.OperationsRepository.Client/CashOperations/TransferOperationsRepositoryClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Log;
+using Lykke.Service.OperationsRepository.AutorestClient;
+using Lykke.Service.OperationsRepository.AutorestClient.Models;
+using Lykke.Service.OperationsRepository.Client.Abstractions.CashOperations;
+using Lykke.Service.OperationsRepository.Client.Models.CashOperations;
+
+namespace Lykke.Service.OperationsRepository.Client.CashOperations
+{
+    public class TransferOperationsRepositoryClient: ITransferOperationsRepositoryClient, IDisposable
+    {
+        private readonly ILog _log;
+        private OperationsRepositoryAPI _apiClient;
+
+        public TransferOperationsRepositoryClient(string serviceUrl, ILog log)
+        {
+            _log = log;
+            _apiClient = new OperationsRepositoryAPI(new Uri(serviceUrl));
+        }
+
+        public void Dispose()
+        {
+            if (_apiClient == null)
+                return;
+            _apiClient.Dispose();
+            _apiClient = null;
+        }
+
+        public async Task<TransferEventResponse> RegisterAsync(TransferEvent transferEvent)
+        {
+            var response = await _apiClient.TransferOperations.RegisterWithHttpMessagesAsync(transferEvent);
+
+            return TransferEventResponse.Prepare(response);
+        }
+
+        public async Task<TransferEventsResponse> GetAsync(string clientId)
+        {
+            var response = await _apiClient.TransferOperations.GetWithHttpMessagesAsync(clientId);
+
+            return TransferEventsResponse.Prepare(response);
+        }
+
+        public async Task<TransferEventResponse> GetAsync(string clientId, string id)
+        {
+            var response = await _apiClient.TransferOperations.GetByRecordIdWithHttpMessagesAsync(clientId, id);
+
+            return TransferEventResponse.Prepare(response);
+        }
+
+        public async Task UpdateBlockChainHashAsync(string clientId, string id, string blockChainHash)
+        {
+            await _apiClient.TransferOperations.UpdateBlockchainHashWithHttpMessagesAsync(clientId, id, blockChainHash);
+        }
+
+        public async Task SetBtcTransactionAsync(string clientId, string id, string btcTransaction)
+        {
+            await _apiClient.TransferOperations.SetBtcTransactionWithHttpMessagesAsync(clientId, id, btcTransaction);
+        }
+
+        public async Task SetIsSettledIfExistsAsync(string clientId, string id, bool offchain)
+        {
+            await _apiClient.TransferOperations.SetIsSettledIfExistsWithHttpMessagesAsync(clientId, id, offchain);
+        }
+
+        public async Task<TransferEventsResponse> GetByHashAsync(string blockchainHash)
+        {
+            var response = await _apiClient.TransferOperations.GetByHashWithHttpMessagesAsync(blockchainHash);
+
+            return TransferEventsResponse.Prepare(response);
+        }
+
+        public async Task<TransferEventsResponse> GetByMultisigAsync(string multisig)
+        {
+            var response = await _apiClient.TransferOperations.GetByMultisigWithHttpMessagesAsync(multisig);
+
+            return TransferEventsResponse.Prepare(response);
+        }
+
+        public async Task<TransferEventsResponse> GetByMultisigsAsync(string[] multisigs)
+        {
+            var response = await _apiClient.TransferOperations.GetByMultisigsWithHttpMessagesAsync(multisigs);
+
+            return TransferEventsResponse.Prepare(response);
+        }
+    }
+}
