db03ba988eb4009250cba05938068fc7d69dfea6
atarutin <tarutin_andrew@mail.ru>
2017-08-08 01:00:24 +0200

    model updated

--- src/Lykke.Service.OperationsRepository/Models/CashOutAttempt/InsertRequestModel.cs
+++ src/Lykke.Service.OperationsRepository/Models/CashOutAttempt/InsertRequestModel.cs
@@ -4,19 +4,18 @@ using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Lykke.Service.OperationsRepository.AzureRepositories.CashOperations;
 using Lykke.Service.OperationsRepository.Core.CashOperations;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Lykke.Service.OperationsRepository.Models.CashOutAttempt
 {
     public class InsertRequestModel
     {
         [Required]
-        public CashOutAttemptEntity Request;
+        public CashOutAttemptEntity Request { get; set; }
         [Required]
-        public PaymentSystem PaymentSystem;
+        public PaymentSystem PaymentSystem { get; set; }
         [Required]
-        public object PaymentFields;
+        public object PaymentFields { get; set; }
         [Required]
-        public CashOutRequestTradeSystem TradeSystem;
+        public CashOutRequestTradeSystem TradeSystem { get; set; }
     }
 }
