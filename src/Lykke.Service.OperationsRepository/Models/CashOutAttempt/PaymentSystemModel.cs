2ba331083251df47e41e09d8e6c441e75e571955
atarutin <tarutin_andrew@mail.ru>
2017-08-08 18:24:24 +0200

    fixed InsertRequestAsync in CashOutAttemptRepositoryController

---
+++ src/Lykke.Service.OperationsRepository/Models/CashOutAttempt/PaymentSystemModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Service.OperationsRepository.Models.CashOutAttempt
+{
+    public class PaymentSystemModel
+    {
+        public string Value { get; set; }
+    }
+}
