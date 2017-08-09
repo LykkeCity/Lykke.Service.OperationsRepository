54fb1ae1bc5b42cc354dbea0be61ccaf9dad771f
atarutin <tarutin_andrew@mail.ru>
2017-08-09 21:19:48 +0200

    added request timeout option

--- client/Lykke.Service.OperationsRepository.Client/CashOperations/CashOutAttemptRepositoryClient.cs
+++ client/Lykke.Service.OperationsRepository.Client/CashOperations/CashOutAttemptRepositoryClient.cs
@@ -16,10 +16,14 @@ namespace Lykke.Service.OperationsRepository.Client.CashOperations
         private readonly ILog _log;
         private OperationsRepositoryAPI _apiClient;
 
-        public CashOutAttemptRepositoryClient(string serviceUrl, ILog log)
+        public CashOutAttemptRepositoryClient(string serviceUrl, ILog log, int timeout)
         {
             _log = log;
-            _apiClient = new OperationsRepositoryAPI(new Uri(serviceUrl));
+            _apiClient =
+                new OperationsRepositoryAPI(new Uri(serviceUrl))
+                {
+                    HttpClient = {Timeout = TimeSpan.FromSeconds(timeout)}
+                };
         }
 
         public void Dispose()
