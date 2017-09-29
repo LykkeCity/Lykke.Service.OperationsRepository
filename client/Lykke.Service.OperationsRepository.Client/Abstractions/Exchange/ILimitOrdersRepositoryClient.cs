b04bc30dc953b9344657a9ba782604674d797893
Dafinka Gerdjikova <dafinka.gerdjikova@programista.pro>
2017-09-29 11:08:38 +0300

    Add clients for extacting lomit orders, market orders and trade events

---
+++ client/Lykke.Service.OperationsRepository.Client/Abstractions/Exchange/ILimitOrdersRepositoryClient.cs
@@ -0,0 +1,14 @@
+﻿using Lykke.Service.OperationsRepository.AutorestClient.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lykke.Service.OperationsRepository.Client.Abstractions.Exchange
+{
+    public interface ILimitOrdersRepositoryClient
+    {
+        Task<LimitOrder> GetOrderAsync(string orderId);
+        Task<IEnumerable<LimitOrder>> GetActiveOrdersAsync(string clientId);
+        Task<IEnumerable<LimitOrder>> GetOrdersAsync(string clientId);
+        Task<IEnumerable<LimitOrder>> GetOrdersAsync(string[] orderIds);
+    }
+}
---
+++ client/Lykke.Service.OperationsRepository.Client/Models/Exchange/MarketOrderResponse.cs
@@ -0,0 +1,45 @@
+﻿using Lykke.Service.OperationsRepository.AutorestClient.Models;
+using Lykke.Service.OperationsRepository.Client.Models.CashOperations;
+using Microsoft.Rest;
+
+namespace Lykke.Service.OperationsRepository.Client.Models.Exchange
+{
+    class MarketOrderResponse : BaseCashOperationResponse<MarketOrder>
+    {
+        public MarketOrder Operation { get; set; }
+
+        public static MarketOrderResponse NullResponse => new MarketOrderResponse { Operation = null };
+
+        public static MarketOrderResponse Prepare(HttpOperationResponse<object> apiResponse)
+        {
+            var error = apiResponse.Body as ErrorResponse;
+            var result = apiResponse.Body as MarketOrder;
+
+            if (error != null)
+            {
+                return new MarketOrderResponse
+                {
+                    Error = new ErrorModel
+                    {
+                        Message = error.ErrorMessage
+                    }
+                };
+            }
+
+            if (result != null)
+            {
+                return new MarketOrderResponse
+                {
+                    Operation = result
+                };
+            }
+
+            return NullResponse;
+        }
+
+        public override MarketOrder GetPayload()
+        {
+            return Operation;
+        }
+    }
+}
